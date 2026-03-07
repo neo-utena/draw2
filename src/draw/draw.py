b80504e2fad5258b8a0ad2055568fe213455e517
HichTala <98521878+HichTala@users.noreply.github.com>
2026-03-07 14:22:42 +0100

    refactor: update draw processing logic for improved readability and maintainability

--- src/draw/draw.py
+++ src/draw/draw.py
@@ -34,7 +34,7 @@ class Draw:
         yolo_path = hf_hub_download(repo_id="HichTala/draw2", filename="ygo_yolo.pt")
 
         model_regression = YOLO(yolo_path)
-        self.results = model_regression.track(
+        self.results = model_regression.predict(
             source=source,
             show_labels=False,
             save=False,
@@ -70,75 +70,75 @@ class Draw:
         if show:
             outputs['image'] = result.orig_img.copy()
 
-        if result.obb.id is not None:
-            for nbox, boxe in enumerate(result.obb.xyxyxyxyn):
-                boxe = np.float32(
-                    [[b[0] * result.orig_img.shape[1], b[1] * result.orig_img.shape[0]] for b in boxe.cpu()]
-                )
-                obb = np.intp(boxe)
-                xy1, _, xy2, _ = obb
+        # if result.obb.id is not None:
+        for nbox, boxe in enumerate(result.obb.xyxyxyxyn):
+            boxe = np.float32(
+                [[b[0] * result.orig_img.shape[1], b[1] * result.orig_img.shape[0]] for b in boxe.cpu()]
+            )
+            obb = np.intp(boxe)
+            xy1, _, xy2, _ = obb
 
-                output_pts = np.float32([
-                    [224, 224],
-                    [224, 0],
-                    [0, 0],
-                    [0, 224]
-                ])
-                perspective_transform = cv2.getPerspectiveTransform(boxe, output_pts)
-                roi = cv2.warpPerspective(
-                    result.orig_img, perspective_transform, (224, 224), flags=cv2.INTER_LINEAR
-                )
-                contours = utils.extract_contours(
-                    roi,
-                    d=self.configs["bilateral_filter_d"],
-                    sigma_color=self.configs["bilateral_filter_sigma_color"],
-                    sigma_space=self.configs["bilateral_filter_sigma_space"],
-                    thresh=self.configs["txt_box_contour_threshold"]
-                )
+            output_pts = np.float32([
+                [224, 224],
+                [224, 0],
+                [0, 0],
+                [0, 224]
+            ])
+            perspective_transform = cv2.getPerspectiveTransform(boxe, output_pts)
+            roi = cv2.warpPerspective(
+                result.orig_img, perspective_transform, (224, 224), flags=cv2.INTER_LINEAR
+            )
+            contours = utils.extract_contours(
+                roi,
+                d=self.configs["bilateral_filter_d"],
+                sigma_color=self.configs["bilateral_filter_sigma_color"],
+                sigma_space=self.configs["bilateral_filter_sigma_space"],
+                thresh=self.configs["txt_box_contour_threshold"]
+            )
 
-                if contours != ():
+            if contours != ():
 
-                    if show:
-                        cv2.drawContours(outputs['image'], [obb], 0, (152, 255, 119), 2)
+                if show:
+                    cv2.drawContours(outputs['image'], [obb], 0, (152, 255, 119), 2)
 
-                    contour = contours[np.array(list(map(cv2.contourArea, contours))).argmax()]
-                    box_txt, txt_aspect_ratio = utils.get_txt(contour)
+                contour = contours[np.array(list(map(cv2.contourArea, contours))).argmax()]
+                box_txt, txt_aspect_ratio = utils.get_txt(contour)
 
-                    rotation = utils.get_rotation(boxes=result.obb.xywhr[nbox], box_txt=box_txt)
-                    if rotation is None:
-                        break
+                rotation = utils.get_rotation(boxes=result.obb.xywhr[nbox], box_txt=box_txt)
+                if rotation is None:
+                    break
 
-                    if rotation != 0:
-                        roi = cv2.rotate(roi, rotation)
+                if rotation != 0:
+                    roi = cv2.rotate(roi, rotation)
 
-                    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-                    roi = Image.fromarray(roi)
+                roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+                roi = Image.fromarray(roi)
 
-                    output = self.classifier(roi, top_k=15)
-                    if output[0]['score'] >= self.confidence_threshold / 100:
-                        if len(self.decklist) == 0:
-                            if display:
-                                outputs['predictions'].append(output[0]['label'])
-                            if show:
-                                cv2.putText(outputs['image'], ' '.join(output[0]['label'].split('-')[:-1]),
-                                            (xy1[0], xy1[1]),
-                                            cv2.FONT_HERSHEY_PLAIN,
-                                            1.0,
-                                            (255, 255, 255),
-                                            2)
+                output = self.classifier(roi, top_k=15)
+                if output[0]['score'] >= self.confidence_threshold / 100:
+                    if len(self.decklist) == 0:
+                        if display:
+                            outputs['predictions'].append(output[0]['label'])
+                        if show:
+                            cv2.putText(outputs['image'], ' '.join(output[0]['label'].split('-')[:-1]),
+                                        (xy1[0], xy1[1]),
+                                        cv2.FONT_HERSHEY_PLAIN,
+                                        1.0,
+                                        (255, 255, 255),
+                                        2)
 
-                        else:
-                            for label in output:
-                                label = label['label']
-                                if label.split('-')[-1] in self.decklist:
-                                    if display:
-                                        outputs['predictions'].append(output[0]['label'])
-                                    if show:
-                                        cv2.putText(outputs['image'], ' '.join(output[0]['label'].split('-')[:-1]),
-                                                    (xy1[0], xy1[1]),
-                                                    cv2.FONT_HERSHEY_PLAIN,
-                                                    1.0,
-                                                    (255, 255, 255),
-                                                    2)
-                                    break
+                    else:
+                        for label in output:
+                            label = label['label']
+                            if label.split('-')[-1] in self.decklist:
+                                if display:
+                                    outputs['predictions'].append(output[0]['label'])
+                                if show:
+                                    cv2.putText(outputs['image'], ' '.join(output[0]['label'].split('-')[:-1]),
+                                                (xy1[0], xy1[1]),
+                                                cv2.FONT_HERSHEY_PLAIN,
+                                                1.0,
+                                                (255, 255, 255),
+                                                2)
+                                break
         return outputs
